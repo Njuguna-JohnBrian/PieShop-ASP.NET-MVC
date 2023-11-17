f900889c3fd91ec29a741c8f8bbeef1467d7ec0c
Njuguna John Brian <njugunajb96@gmail.com>
2023-11-17 14:59:41 +0300

    create Pie repository

---
+++ WeShop/Models/IPieRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace WeShop.Models;
+
+public interface IPieRepository
+{
+    IEnumerable<Pie> AllPies { get; }
+    IEnumerable<Pie> PiesOfTheWeek { get; }
+    Pie? GetPieById(int pieId);
+}
