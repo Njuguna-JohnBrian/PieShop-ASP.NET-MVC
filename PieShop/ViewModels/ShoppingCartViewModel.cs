723f057cb5fccaa00498d9c01a3304473b5cd5f9
Njuguna John Brian <njugunajb96@gmail.com>
2023-11-18 21:49:25 +0300

    add shopping cart view model

---
+++ PieShop/ViewModels/ShoppingCartViewModel.cs
@@ -0,0 +1,15 @@
+using PieShop.Models;
+
+namespace PieShop.ViewModels;
+
+public class ShoppingCartViewModel
+{
+    public IShoppingCart ShoppingCart { get; }
+    public decimal ShoppingCartTotal { get; }
+
+    public ShoppingCartViewModel(IShoppingCart shoppingCart, decimal shoppingCartTotal)
+    {
+        ShoppingCart = shoppingCart;
+        ShoppingCartTotal = shoppingCartTotal;
+    }
+}
