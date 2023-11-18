d04e2f56c5f68df5de95eaf5c576dffc1f8e47e8
Njuguna John Brian <njugunajb96@gmail.com>
2023-11-18 11:12:47 +0300

    get all pies

--- WeShop/Controllers/PieController.cs
+++ WeShop/Controllers/PieController.cs
@@ -1,7 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WeShop.Models;
 
 namespace WeShop.Controllers;
 
 public class PieController : Controller
 {
+    private readonly IPieRepository _pieRepository;
+    private readonly ICategoryRepository _categoryRepository;
+
+    public PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository)
+    {
+        _pieRepository = pieRepository;
+        _categoryRepository = categoryRepository;
+    }
+
+    public IActionResult List()
+    {
+        return View(_pieRepository.AllPies);
+    }
 }
