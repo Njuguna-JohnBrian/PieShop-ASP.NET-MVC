92bf7d9de1b35d7089c2aa3c68e8e0510c45e25b
Njuguna John Brian <njugunajb96@gmail.com>
2023-11-18 20:41:29 +0300

    add amount column

--- PieShop/Models/ShoppingCartItem.cs
+++ PieShop/Models/ShoppingCartItem.cs
@@ -4,5 +4,6 @@ public class ShoppingCartItem
 {
     public int ShoppingCartItemId { get; set; }
     public Pie Pie { get; set; } = default!;
+    public int Amount { get; set; }
     public string? ShoppingCartId { get; set; }
 }
